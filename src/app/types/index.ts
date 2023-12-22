13ec2c78317f4050fe73958e9709b7f86c573808
Fernando Barreto <fernando200040@hotmail.com>
2023-12-22 13:24:55 -0300

    doc: remove extra comment

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,29 +1,28 @@
-// src/types/index.ts
 export interface Holiday {
-  id: string;
-  startDate: string;
-  endDate: string;
-  type: string;
-  name: Array<{ language: string; text: string }>;
-  nationwide: boolean;
-  subdivisions: Array<{ code: string; shortName: string }>;
+   id: string;
+   startDate: string;
+   endDate: string;
+   type: string;
+   name: Array<{ language: string; text: string }>;
+   nationwide: boolean;
+   subdivisions: Array<{ code: string; shortName: string }>;
 }
 export interface Canton {
-  isoCode: string;
-  name: [{ language: string; text: string }];
+   isoCode: string;
+   name: [{ language: string; text: string }];
 }
 
 export interface SearchState {
-  selectedCanton: string | null;
-  dateRange: [string | null, string | null];
+   selectedCanton: string | null;
+   dateRange: [string | null, string | null];
 }
 export interface HolidaySearchContextState {
-  cantons: Canton[];
-  holidays: Holiday[];
-  searchState: SearchState;
-  selectedCanton: string | null;
-  setSelectedCanton: (isoCode: string | null) => void;
-  setDateRange: ([startDate, endDate]: [string | null, string | null]) => void;
-  fetchHolidays: () => void;
-  loading: boolean;
+   cantons: Canton[];
+   holidays: Holiday[];
+   searchState: SearchState;
+   selectedCanton: string | null;
+   setSelectedCanton: (isoCode: string | null) => void;
+   setDateRange: ([startDate, endDate]: [string | null, string | null]) => void;
+   fetchHolidays: () => void;
+   loading: boolean;
 }
